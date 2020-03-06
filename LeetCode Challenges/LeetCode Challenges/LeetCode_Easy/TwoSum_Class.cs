7a5d8cadbacf68dd32cae20cf03edf811da2e08b
Tyler Moore <tyler.moore53@outlook.com>
2020-03-05 17:33:29 -0800

    Commit

---
+++ LeetCode Challenges/LeetCode Challenges/LeetCode_Easy/TwoSum_Class.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Challenges
+{
+    class TS
+    {
+        //To find the targeted value between the index values in an array.
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            //Pull the first index value in the array and compare it to the following index values.
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    //put the index values in the array
+                    int[] arr = { i, j };
+                    //if the index value's sum equal the target return the index.
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return arr;
+                    }
+                }
+
+            }
+            //return null if it does not equate to the target number.
+            return null;
+        }
+        //The first attempt to find the targeted value between the index values in an array.
+        //Does Not Work All The Time!
+        public static int[] TwoSum0(int[] nums, int target)
+        {
+            //initialize a count and incriment it through the loop until the values match.
+            int count = 0;
+            while (true)
+            {
+                int[] arr = { count, count + 1 };
+                if (nums[count] + nums[count + 1] != target)
+                {
+                    count++;
+                }
+                if (nums[count] + nums[count + 1] == target)
+                {
+                    return arr;
+                }
+
+            }
+        }
+    }
+}
---
+++ LeetCode Challenges/LeetCode Challenges/MainProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace LeetCode_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] test1 = { 3,2,4 };//Test array
+            foreach(int i in TS.TwoSum(test1,6))//This is to display each index in the array that had values that added up to the target value.
+            {
+                Console.WriteLine(i);//
+            }
+            Console.WriteLine(TS.TwoSum(test1,6));//To show the value/datatype returned.
+
+           
+        }
+
+ 
+    }
+}
