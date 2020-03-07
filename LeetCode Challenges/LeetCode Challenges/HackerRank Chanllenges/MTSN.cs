ef9276649eb27d570392bb8f838fbe4e6afe5e95
Tyler Moore <tyler.moore53@outlook.com>
2020-03-07 10:59:07 -0800

    Commit CompareThrees

---
+++ LeetCode Challenges/LeetCode Challenges/HackerRank Chanllenges/MTSN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Challenges
+{
+    class MTSN
+    {
+        public static List<int> CompareThrees(List<int> a, List<int> b)
+        {
+            List<int> count = new List<int>() { 0, 0};
+            for(int i = 0; i< a.Count; i++)
+            {
+                if (a[i] == b[i])
+                {
+                    continue;
+                }
+                else if (a[i] > b[i])
+                {
+                    count[0] = count[0] + 1;
+                }
+                else if (a[i] < b[i])
+                {
+                    count[1] = count[1] + 1;
+                }
+            }
+            return count;
+        }
+
+    }
+
+}
--- LeetCode Challenges/LeetCode Challenges/MainProgram.cs
+++ LeetCode Challenges/LeetCode Challenges/MainProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace LeetCode_Challenges
 {
@@ -8,14 +9,25 @@ namespace LeetCode_Challenges
         static void Main(string[] args)
         {
 
+
+            int[] test = { 2, 2, 4 };//Test array
             int[] test1 = { 3,2,4 };//Test array
-            foreach(int i in TS.TwoSum(test1,6))//This is to display each index in the array that had values that added up to the target value.
+
+            List<int> test2 = new List<int> () { 1, 2, 4, 4 };
+            List<int> test3 = new List<int>() { 2, 3, 3, 4 };
+            //foreach(int i in TS.TwoSum(test1,6))//This is to display each index in the array that had values that added up to the target value.
+            //{
+            //    Console.WriteLine(i);//
+            //}
+            //Console.WriteLine(TS.TwoSum(test1,6));//To show the value/datatype returned.
+
+            foreach (int i in MTSN.CompareThrees(test2, test3))//This is to display each index in the array that had values that added up to the target value.
             {
                 Console.WriteLine(i);//
             }
-            Console.WriteLine(TS.TwoSum(test1,6));//To show the value/datatype returned.
 
-           
+            Console.WriteLine(MTSN.CompareThrees(test2, test3));
+
         }
 
  
